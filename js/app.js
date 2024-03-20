c99808094a3e4c892f1a9c38fd3829e5b30a24bb
Mariana Tello <marianafernandatello@gmail.com>
2024-03-19 21:13:55 -0300

    pre-entrega 2

--- js/app.js
+++ js/app.js
@@ -1,51 +1,34 @@
-let pais = prompt("Ingresa el pais de America del sur que quieras visitar");
+let pais = prompt("Ingresa el pais que quieras visitar");
 
-function argentina() {
-    pais = pais.toLocaleLowerCase();
-  switch (pais) {
-    case "argentina":
-      alert("Bien! Vamos!!!");
-      break;
-    case "brasil":
-      alert("Bien! Vamos!!!");
-      break;
-    case "chile":
-      alert("Bien! Vamos!!!");
-      break;
-    case "uruguay":
-      alert("Bien! Vamos!!!");
-      break;
-    case "paraguay":
-      alert("Bien! Vamos!!!");
-      break;
-    case "bolivia":
-      alert("Bien! Vamos!!!");
-      break;
-    case "peru":
-      alert("Bien! Vamos!!!");
-      break;
-    case "ecuador":
-      alert("Bien! Vamos!!!");
-      break;
-    case "colombia":
-      alert("Bien! Vamos!!!");
-      break;
-    case "venezuela":
-      alert("Bien! Vamos!!!");
-      break;
-    case "guyana":
-      alert("Bien! Vamos!!!");
-      break;
-    case "surinam":
-      alert("Bien! Vamos!!!");
-      break;
-    case "guayana francesa":
-      alert("Bien! Vamos!!!");
-      break;
-    default:
-      alert("Ese pais no pertenece a America del Sur");
-      break;
+const mensaje = (element) => {
+  if (element) {
+    alert(
+      "Encontramos un paquete ideal para ti en " +
+        element.nombre +
+        " USD " +
+        element.valor +
+        " por persona"
+    );
+  } else {
+    alert(
+      "No tenemos paquetes disponibles para ese destino, intenta con : Argentina, España o Inglaterra"
+    );
+  }
+};
+
+function busquedaPaises() {
+  pais = pais.toLocaleLowerCase();
+  if (pais) {
+    for (let index = 0; index < paises.length; index++) {
+      const element = paises[index];
+
+      if (pais === element.nombre) {
+        return mensaje(element);
+      } else if (paises.length < index + 2) {
+        return mensaje();
+      }
+    }
   }
 }
 
-argentina();
+busquedaPaises();
